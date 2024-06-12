9e72b8cfd3eb6c8b63e5dd3b1b39a638fdeb357a
anonymousrecords <a.Records2022@gmail.com>
2024-06-12 21:23:46 +0900

    feat : helmet 설정

--- src/Intro/Intro.tsx
+++ src/Intro/Intro.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
 function Intro() {
   return (
     <div>
+      <Helmet>
+        <title>Intro</title>
+      </Helmet>
       Intro
     </div>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
--- src/Onboarding/Onboarding.tsx
+++ src/Onboarding/Onboarding.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
 function Onboarding() {
   return (
     <div>
+      <Helmet>
+        <title>Onboarding</title>
+      </Helmet>
       Onboarding
     </div>
-  )
+  );
 }
 
-export default Onboarding
+export default Onboarding;
