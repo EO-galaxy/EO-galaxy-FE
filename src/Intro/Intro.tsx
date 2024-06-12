bfe0c48ccec63f67e1e1e0386eafc8e134e507f9
anonymousrecords <a.Records2022@gmail.com>
2024-06-12 21:44:22 +0900

    feat : 공유하기 기능 초안 구현

--- src/Intro/Intro.tsx
+++ src/Intro/Intro.tsx
@@ -2,12 +2,32 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 
 function Intro() {
+  async function handleShareLink() {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: "EO Galaxy",
+          text: "EO Galaxy",
+          url: "https://eo-galaxy.vercel.app/",
+        });
+        console.log("Success share link");
+      } catch (error) {
+        console.error("Error sharing link", error);
+      }
+    } else {
+      console.error("Web Share API is not supported in this browser.");
+      alert("링크 공유 기능이 지원되지 않는 브라우저입니다.");
+    }
+  };
   return (
     <div>
       <Helmet>
         <title>Intro</title>
       </Helmet>
-      Intro
+      <main>
+        Intro
+        <button onClick={() => handleShareLink()}>링크 공유하기</button>
+      </main>
     </div>
   );
 }
