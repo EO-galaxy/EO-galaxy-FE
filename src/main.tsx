9140957329e6b33801722808585bbd69b5849533
anonymousrecords <a.Records2022@gmail.com>
2024-06-13 17:02:39 +0900

    chore : vercel.json 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import "./util/locale/index";
-import { router } from "./router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout";
+import { Onboarding } from "./Onboarding";
+import Intro from "./Intro/Intro";
+import { Home } from "./Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>{router}</React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Onboarding />} />
+          <Route path="/intro" element={<Intro />} />
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
