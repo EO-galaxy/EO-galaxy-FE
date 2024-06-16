406e5f44c174875c775f42380ce5491884d21e76
anonymousrecords <a.Records2022@gmail.com>
2024-06-16 16:26:21 +0900

    feat : Layout 컴포넌트 구현

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,13 +1,27 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { HelmetProvider } from "react-helmet-async";
 import { Outlet } from "react-router-dom";
+import { Header } from "../components/Header";
 
 export default function Layout() {
   const helmetContext = {};
   return (
     <HelmetProvider context={helmetContext}>
-      <div className="layout">
+      <div css={LayoutStyles}>
+        <Header />
         <Outlet />
       </div>
     </HelmetProvider>
   );
 }
+
+const LayoutStyles = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: var(--bg-secondary);
+`;
