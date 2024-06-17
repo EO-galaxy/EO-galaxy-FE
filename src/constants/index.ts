1fee0884e38d85a5cba9100b4ba1ad8991481474
anonymousrecords <a.Records2022@gmail.com>
2024-06-17 19:36:32 +0900

    chore : 랜덤 추천 영상 더미 데이터 추가

---
+++ src/constants/index.ts
@@ -0,0 +1,64 @@
+import { t } from "i18next";
+
+export const VideoList = [
+  {
+    id: 0,
+    imgSrc: "/intro-image/video-0.jpg",
+    description: t("item_video_0"),
+    link: "https://www.youtube.com/watch?v=0qfqjObM50o",
+  },
+  {
+    id: 1,
+    imgSrc: "/intro-image/video-1.jpg",
+    description: t("item_video_1"),
+    link: "https://www.youtube.com/watch?v=8zfYINYNS38",
+  },
+  {
+    id: 2,
+    imgSrc: "/intro-image/video-2.webp",
+    description: t("item_video_2"),
+    link: "https://www.youtube.com/watch?v=DnwbrjqQNTU",
+  },
+  {
+    id: 3,
+    imgSrc: "/intro-image/video-3.jpg",
+    description: t("item_video_3"),
+    link: "https://www.youtube.com/watch?v=haPB7f7NlCY&t=16s",
+  },
+  {
+    id: 4,
+    imgSrc: "/intro-image/video-4.jpg",
+    description: t("item_video_4"),
+    link: "https://www.youtube.com/watch?v=kAfYEtj8XaU",
+  },
+  {
+    id: 5,
+    imgSrc: "/intro-image/video-5.jpg",
+    description: t("item_video_5"),
+    link: "https://www.youtube.com/watch?v=xAxllziXPSo",
+  },
+  {
+    id: 6,
+    imgSrc: "/intro-image/video-6.webp",
+    description: t("item_video_6"),
+    link: "https://www.youtube.com/watch?v=3-1kfqoUcpo",
+  },
+  {
+    id: 7,
+    imgSrc: "/intro-image/video-7.jpg",
+    description: t("item_video_7"),
+    link: "https://www.youtube.com/watch?v=V9AGvwPmnZU",
+  },
+  {
+    id: 8,
+    imgSrc: "/intro-image/video-8.jpg",
+    description: t("item_video_8"),
+    link: "https://www.youtube.com/watch?v=vVzLd9Zf4ks",
+  },
+  {
+    id: 9,
+    imgSrc: "/intro-image/video-9.jpg",
+    description: t("item_video_9"),
+    link: "https://www.youtube.com/watch?v=L2sNAQYLNbA",
+  },
+];
