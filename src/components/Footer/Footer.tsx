6cb2bd85845a3f42bc974338b151e73a1405d48a
anonymousrecords <a.Records2022@gmail.com>
2024-06-20 14:23:40 +0900

    fix : footer width 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,15 +3,23 @@ import { css } from "@emotion/react";
 
 function Footer() {
   return (
-    <footer css={FooterStyles}>
-      <p>
-        해당 웹사이트는 단순히 EO 채널을 좋아하는 마음을 담아 만들어졌으며,
-        어떠한 수익을 얻을 계획도 없습니다.
-      </p>
-      <p>
-        문의: <a href="mailto:okdal20@gmail.com" style={{textDecoration: 'underline'}}>okdal20@gmail.com</a>
-      </p>
-    </footer>
+    <div style={{ width: "100%" }}>
+      <footer css={FooterStyles}>
+        <p>
+          해당 웹사이트는 단순히 EO 채널을 좋아하는 마음을 담아 만들어졌으며,
+          어떠한 수익을 얻을 계획도 없습니다.
+        </p>
+        <p>
+          문의:{" "}
+          <a
+            href="mailto:okdal20@gmail.com"
+            style={{ textDecoration: "underline" }}
+          >
+            okdal20@gmail.com
+          </a>
+        </p>
+      </footer>
+    </div>
   );
 }
 
