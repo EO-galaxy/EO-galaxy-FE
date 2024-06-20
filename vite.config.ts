bed0cdc41dd9f5f19429582dea50bba79c1547c4
anonymousrecords <a.Records2022@gmail.com>
2024-06-20 16:38:57 +0900

    chore : favicon 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
-import history from "connect-history-api-fallback";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteFaviconsPlugin({
-      logo: "public/logo.png",
-    }),
   ],
 });
