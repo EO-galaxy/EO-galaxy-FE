3dff39a753c3310740e0f020034ba7ae52c1be68
anonymousrecords <a.Records2022@gmail.com>
2024-06-20 14:39:23 +0900

    fix : vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { ViteFaviconsPlugin } from "vite-plugin-favicon";
+import history from "connect-history-api-fallback";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,14 @@ export default defineConfig({
     ViteFaviconsPlugin({
       logo: "public/logo.png",
     }),
+    {
+      name: "single-page-app",
+      configureServer(server) {
+        const middleware = history();
+        server.middlewares.use((req, res, next) => {
+          middleware(req as any, res as any, next);
+        });
+      },
+    },
   ],
 });
